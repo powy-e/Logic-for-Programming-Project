4e187f4acc704c673d6a28c3459cef4b555f9ce6
powy-e <86477141+powy-e@users.noreply.github.com>
2022-01-25 22:48:31 +0000

    DOCSTRINGADO

--- EN_10241.pl
+++ EN_10241.pl
@@ -1,41 +1,65 @@
-% IDENTIFICACAO E COISA E TAL
 
-% Extrai_ilhas_Linhas(Numero da Linha, Linha, Lista de Ilhas existentes)/3
-% Permite extrair as ilhas de uma linha
+%             Logica para Programacao
+%      Projeto - Solucionador de Puzzles Hashi
+%
+%             Eduardo Nazario - 102415
+%        eduardo.nazario@tecnico.ulisboa.pt
+
+
+%------------------------------------------------------------------------------
+
+% 2.1
+% Predicado extrai_ilhas_Linhas/3
+% Permite obter dada uma Linha e o seu numero, as ilhas incluidas nessa linha,
+
+%------------------------------------------------------------------------------
+
 extrai_ilhas_linha_aux(_, _, Ilhas, Index, L, Ilhas) :- Index > L.
 extrai_ilhas_linha_aux(N_L, Linha, Ilhas, Index, L, F_Ilhas) :- 
     nth1(Index, Linha, P),
-     P=\= 0,
-     append(Ilhas, [ilha(P,(N_L,Index))], Novas_ilhas),
-     N_index is Index + 1, 
-     extrai_ilhas_linha_aux(N_L, Linha, Novas_ilhas, N_index, L, F_Ilhas);
-
-     N_index is Index + 1, 
-     extrai_ilhas_linha_aux(N_L, Linha, Ilhas, N_index, L, F_Ilhas).
+    P=\= 0,
+    append(Ilhas, [ilha(P,(N_L,Index))], Novas_ilhas),
+    N_index is Index + 1, 
+    extrai_ilhas_linha_aux(N_L, Linha, Novas_ilhas, N_index, L, F_Ilhas);
+    
+    N_index is Index + 1, 
+    extrai_ilhas_linha_aux(N_L, Linha, Ilhas, N_index, L, F_Ilhas).
 
 
 extrai_ilhas_linha(_, [], []). %REDUNDANTE???? WHO KNOWS NOT SHY NOT ME ITZY
 extrai_ilhas_linha(N_L, Linha, Ilhas) :-
     length(Linha, L), 
-     extrai_ilhas_linha_aux(N_L, Linha, [], 1, L, Ilhas).
+    extrai_ilhas_linha_aux(N_L, Linha, [], 1, L, Ilhas).
 
 
 
 
-% Predicado ilhas(Puz, Ilhas)/2 premite a partir de um Puzzle obter uma lista
-% com Ilhas ordenada da esquerda p/a a direita e debaixo p/a cima
+%------------------------------------------------------------------------------
+
+% 2.2
+% Predicado ilhas/2
+% Permite com um Puzzle obter uma lista de Ilhas ordenadas,
+% isto e da esquerda para a direita e debaixo para cima
+
+%------------------------------------------------------------------------------
+
 ilhas_aux(_, Ilhas_Acc, N_Linha, Ilhas_Acc, L):- N_Linha > L.
 ilhas_aux(Puz, Ilhas_prev, N_Linha, Ilhas_Final, L):- 
     nth1(N_Linha, Puz, Linha), extrai_ilhas_linha(N_Linha, Linha, Ilhas),
-     NovoN_Linha is N_Linha +1,
-     append(Ilhas_prev, Ilhas, Ilhas_Acc),
-     ilhas_aux(Puz, Ilhas_Acc, NovoN_Linha, Ilhas_Final, L).
+    NovoN_Linha is N_Linha +1,
+    append(Ilhas_prev, Ilhas, Ilhas_Acc),
+    ilhas_aux(Puz, Ilhas_Acc, NovoN_Linha, Ilhas_Final, L).
 
 ilhas(Puz, Ilhas) :-
     length(Puz, L), ilhas_aux(Puz, [], 1, Ilhas, L).
 
 
-% VIZINHAS
+%------------------------------------------------------------------------------
+
+% 2.3
+% Predicado vizinhas/3
+% Permite obter quais as Ilhas que são Vizinhas de uma dada ilha
+
 
 mesma_ilha(L, C, ilha(_,(L,C))).
 mesma_linha(L,ilha(_,(L,_))).
@@ -58,18 +82,18 @@ escolhe_elementos_prox_verticais(_, [], El_Acc, El_Acc).
 escolhe_elementos_prox_verticais(L, [ilha(N,(LI,C))|R_Ilhas], V_Linhas, Linhas_Acc) :-
     L < LI,  append(Linhas_Acc, [ilha(N,(LI,C))], V_Linhas);
     L > LI,  escolhe_elementos_prox_verticais(L, R_Ilhas, V_Linhas, [ilha(N,(LI,C))]).
- 
+
 junta_linha_coluna(Horizontais, Verticais, L, Vizinhas) :-
     length(Verticais, 0), Vizinhas = Horizontais;
     length(Verticais, 1),
-     Verticais = [ilha(_,(L1,_))],
-     L>L1,
-     append(Verticais, Horizontais, Vizinhas);
+    Verticais = [ilha(_,(L1,_))],
+    L>L1,
+    append(Verticais, Horizontais, Vizinhas);
     length(Verticais, 1),
-     append(Horizontais, Verticais, Vizinhas);
+    append(Horizontais, Verticais, Vizinhas);
     Verticais = [P|R], append([P], Horizontais, Pre), append(Pre, R, Vizinhas).
-    
-    
+
+
 vizinhas([], _,[]).
 vizinhas(Ilhas, ilha(_,(L,C)), Vizinhas) :-
     exclude(mesma_ilha(L,C), Ilhas, F_Ilhas), %Remove o proprio elemento
@@ -80,9 +104,14 @@ vizinhas(Ilhas, ilha(_,(L,C)), Vizinhas) :-
     junta_linha_coluna(V_horizontais, V_verticais, L, Vizinhas).
 
 
+%------------------------------------------------------------------------------
 
-%% Estado
+% 2.4
+% Predicado estado/2
+% Permite originar uma lista com 3 elementos, uma Ilha, uma Lista com as
+% Vizinhas dessa Ilha e uma Lista com pontes da ilha ([] no inicio)
 
+%------------------------------------------------------------------------------
 
 estado_aux(_, Estado_Acumulado, Len_Ilhas, Estado_Acumulado, Len_Ilhas).
 estado_aux(Ilhas, Estado, Index, Estado_Acumulado, Len_Ilhas):-
@@ -99,9 +128,13 @@ estado(Ilhas, Estado) :-
     estado_aux(Ilhas, Estado, 0, [], L).
 
 
+%------------------------------------------------------------------------------
 
-% posicoes_entre
+% 2.5
+% Predicado posicoes_entre/3
+% Permite Obter uma Lista com as posicoes localizadas entre duas posicoes
 
+%------------------------------------------------------------------------------
 
 
 posicoes_entre((L,C1), (L,C2), Posicoes) :-
@@ -109,7 +142,7 @@ posicoes_entre((L,C1), (L,C2), Posicoes) :-
     C_I is CI + 1,
     C_F is CF -1,
     findall((L,C3), between(C_I, C_F, C3), Posicoes).
-    
+
 posicoes_entre((L1,C), (L2,C), Posicoes) :-
     (L1 < L2 -> LI = L1, LF = L2;LI = L2, LF = L1),
     L_I is LI + 1,
@@ -117,15 +150,26 @@ posicoes_entre((L1,C), (L2,C), Posicoes) :-
     findall((L3,C), between(L_I, L_F, L3), Posicoes).
 
 
+%------------------------------------------------------------------------------
 
-%cria_ponte
+% 2.6
+% Predicado cria_ponte/3
+% Permite criar uma Ponte entre duas Posicoes (ponte(POS1, POS2)) 
+
+%------------------------------------------------------------------------------
 
 cria_ponte((L1,C1), (L2,C2), ponte(PosF1, PosF2)):- 
     L1=< L2, C1=< C2, PosF1 = (L1,C1), PosF2 = (L2,C2);
     PosF1 = (L2,C2), PosF2 = (L1,C1).
 
 
-% caminho_livre/5
+%------------------------------------------------------------------------------
+
+% 2.7
+% Predicado caminho_livre/5
+% Permite verificar se uma ponte nao obstroi duas Ilhas Vizinhas
+
+%------------------------------------------------------------------------------
 
 caminho_livre(Pos1, Pos2, _, ilha(_,(L1,C1)), ilha(_,(L2,C2))):-
     member((L1,C1),[Pos1, Pos2]), member((L2,C2),[Pos1,Pos2]).
@@ -135,15 +179,27 @@ caminho_livre(_, _, Posicoes, ilha(_,(L1,C1)), ilha(_,(L2,C2))):-
     Ocupado = [].
 
 
+%------------------------------------------------------------------------------
 
-% atualiza_vizinhas_entrada/5
+% 2.8
+% Predicado atualiza_vizinhas_entrada/5
+% Permite atualizar uma entrada para que ilhas deixem de ser Vizinhas
+% tiverem o caminho obstruido por uma ponte
+
+%------------------------------------------------------------------------------
 
 actualiza_vizinhas_entrada(Pos1, Pos2, Posicoes, [I,Vz, Pt], [I, N_Vz, Pt]):-
     findall(Y, (member(Y,Vz), caminho_livre(Pos1, Pos2, Posicoes, I, Y)), N_Vz).
-    
 
 
-% actualiza_vizinhas_apos_pontes/4
+%------------------------------------------------------------------------------
+
+% 2.9
+% Predicado actualiza_vizinhas_apos_pontes/4
+% Permite obter o Estado apos a atualizacao das ilhas que deixam de ser Vizinhas
+% com a construcao de uma ponte
+
+%------------------------------------------------------------------------------
 
 actualiza_vizinhas_apos_pontes([], _, _, []).
 actualiza_vizinhas_apos_pontes(Estado, Pos1, Pos2, [[I,Vz, Pt]]):-
@@ -155,49 +211,100 @@ actualiza_vizinhas_apos_pontes([P|R], Pos1, Pos2, NovoEstado):-
     actualiza_vizinhas_entrada(Pos1, Pos2, Posicoes, P, [I,Vz, Pt]),
     actualiza_vizinhas_apos_pontes(R, Pos1, Pos2, Estado_seguinte),
     append([[I,Vz, Pt]], Estado_seguinte, NovoEstado).
-    
 
-% ilhas_terminadas/2
+%------------------------------------------------------------------------------
+
+% 2.10
+% Predicado ilhas_terminadas/2
+% Permite obter uma lista com todas as ilhas que ja tem associadas a elas 
+% todas as pontes possiveis de um estado
+
+%------------------------------------------------------------------------------
+
 ilhas_terminadas(Estado, Ilhas_term):-
     findall(
         ilha(N, (L,C)), 
         (
-            member([ilha(N, (L,C)), _, Pontes], Estado), N\='X', length(Pontes, N)
-            ),
-         Ilhas_term).
+        member([ilha(N, (L,C)), _, Pontes], Estado), N\='X', length(Pontes, N)
+        ),
+        Ilhas_term).
+    
+    
+%------------------------------------------------------------------------------
 
+% 2.11 
+% Predicado tira_ilhas_terminadas_entrada/3
+% Permite obter uma entrada com as ilhas terminadas removidas da lista de
+% vizinhas de outra entrada
 
-% 2.11 tira_ilhas_terminadas_entrada/3
+%------------------------------------------------------------------------------
+
 
 membro_ao_contrario(Lista, El):-
     member(El, Lista).
-    
+
 tira_ilhas_terminadas_entrada(Ilhas_term, [I, Vz, Pt], [I, N_Vz, Pt]) :-
     exclude(membro_ao_contrario(Ilhas_term), Vz, N_Vz).
 
-% 2.12 tira_ilhas_terminadas/3
+%------------------------------------------------------------------------------
+
+% 2.12 
+% Predicado tira_ilhas_terminadas/3
+% Permite obter um estado em que todas as ilhas terminadas foram removidas das
+% suas entradas
+
+%------------------------------------------------------------------------------
+
 tira_ilhas_terminadas(Estado, Ilhas_term, Novo_estado) :-
     maplist(tira_ilhas_terminadas_entrada(Ilhas_term), Estado, Novo_estado).
 
+%------------------------------------------------------------------------------
 
-%2.13 Predicado marca_ilhas_terminadas_entrada/3
+% 2.13 
+% Predicado marca_ilhas_terminadas_entrada/3
+% Permite marcar a ilha de uma entrada se esta pertencer a uma lista de ilhas
+% terminadas, isto e substituir o seu numero de pontes por 'X'
+
+%------------------------------------------------------------------------------
 
 marca_ilhas_terminadas_entrada(Ilhas_term,[ilha(N,Pos), Vz, Pt], [ilha(X,Pos), Vz, Pt]):-
     member(ilha(N,Pos), Ilhas_term) -> X='X'; X=N.
-    
-%2.14 Predicado marca_ilhas_terminadas/3
+
+%------------------------------------------------------------------------------
+
+% 2.14 
+% Predicado marca_ilhas_terminadas/3
+% Permite obter um estado em que todas as ilhas terminadas foram marcadas nas
+% suas entradas
+ 
+
+%------------------------------------------------------------------------------
+
 marca_ilhas_terminadas(Estado, Ilhas_term, Novo_estado) :-
     maplist(marca_ilhas_terminadas_entrada(Ilhas_term), Estado, Novo_estado).
 
+%------------------------------------------------------------------------------
 
-%2.15 trata_ilhas_terminadas/2
+% 2.15 
+% Predicado trata_ilhas_terminadas/2
+% Permite obter um estado apos a aplicacao de tira_ilhas_terminadas e
+% marca_ilhas_terminadas a um outro estado
+
+%------------------------------------------------------------------------------
 
 trata_ilhas_terminadas(Estado, Novo_estado):-
     ilhas_terminadas(Estado, Ilhas_term),
     tira_ilhas_terminadas(Estado, Ilhas_term, Meio),
     marca_ilhas_terminadas(Meio, Ilhas_term, Novo_estado).
 
-% 2.16 junta_pontes/5
+%------------------------------------------------------------------------------
+
+% 2.16 
+% Predicado junta_pontes/5
+% Permite obter o estado resultante da adicao de pontes entre duas ilhas
+
+%------------------------------------------------------------------------------
+
 adiciona_multiplas(Coisa, Vezes, List):-
     length(List, Vezes), maplist(=(Coisa), List).
 
@@ -218,6 +325,6 @@ junta_pontes(Estado, Num_Pontes, ilha(N1,(L1,C1)), ilha(N2,(L2,C2)), Novo_Estado
     actualiza_vizinhas_apos_pontes(Estado_Pontes, (L1,C1), (L2,C2), Estado_Atualizado),
     trata_ilhas_terminadas(Estado_Atualizado, Novo_Estado).
 
-    
+
 
 
